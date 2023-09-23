dc2cd51fd9366ac8c69c3cac187fb850f9434bce
Sridhar Iyer <iyer.a.sri@gmail.com>
2023-09-23 21:28:21 +0000

    Finished the random fact generator code

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
 import streamlit as st
+import random
 
-st.title("Hello world!")
+# Function to read facts from "facts.txt" file
+def read_facts(filename):
+    with open(filename, 'r') as file:
+        facts = file.read().split('\n\n')
+    return [fact.strip() for fact in facts if fact.strip()]
+
+# Main Streamlit app
+def main():
+    st.title("Random Fact Generator")
+    
+    # Read facts from the file
+    facts = read_facts("facts.txt")
+    
+    if not facts:
+        st.error("No facts found in the file.")
+    else:
+        # Button to generate a random fact
+        if st.button("Generate A Random Fact"):
+            random_fact = random.choice(facts)
+            # Style the fact text with a nice font and larger font size
+            st.markdown(f"<p style='font-family: Arial; font-size: 20px;'>{random_fact}</p>", unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
